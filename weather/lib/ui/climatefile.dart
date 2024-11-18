7c4fb7dd58292a59810c3262150bf6b4e067eb11
Haseeb-ur-Rehman/009 <choudharyhaseeb86@gmail.com>
2024-11-18 09:47:50 +0500

    video 1 (project setup/ui folder )
    in this video we create a weather project and in lid folder created ui folder with climatefile,dart

---
+++ weather/lib/ui/climatefile.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+class Climate extends StatefulWidget {
+  const Climate({super.key});
+
+  @override
+  State<Climate> createState() => _ClimateState();
+}
+
+class _ClimateState extends State<Climate> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
